20b8e3b953222558812b15206d361468e8d3d4d2
Lupin <mail@lupin.me>
2016-06-10 08:34:09 +0200

    testing the plugin in js file

--- sugarizeros.js
+++ sugarizeros.js
@@ -1,8 +1,18 @@
 
 var sugarizeros;
 
+function onSuccess(message){
+    alert("onSuccess");
+    alert(message);
+}
+
+function onFailure(){
+    alert("onFailure");
+}
+
 function init(){
-    alert("SugarizerOS Loaded");
+//    alert("SugarizerOS Loaded");
+    exec(onSuccess, onFailure, "SugarizerOSPlugin", "echo", ["I'm Alive"]);    
     sugarizeros = true;
 }
 
