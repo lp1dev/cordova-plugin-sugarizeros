9cc94954bb736144b8b5dd0269f6c3c4f25f7483
Lupin <mail@lupin.me>
2016-06-13 15:30:29 +0200

    added method to retrieve icon and apps separatedly and patchs

--- android/SugarizerOSPlugin.java
+++ android/SugarizerOSPlugin.java
@@ -2,49 +2,99 @@ package org.olpcfrance.sugarizer;
 
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
-/**
- * This class echoes a string called from JavaScript.
- */
-
 public class SugarizerOSPlugin extends CordovaPlugin {
+    private PackageManager pm;
 
-    private void getApps(CallbackContext callbackContext){
-	String output = "";
+    private String bitmapToBase64(Bitmap bitmap){
+	ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+	bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+	byte[] byteArray = byteArrayOutputStream .toByteArray();
+	System.out.println(Base64.encodeToString(byteArray, Base64.DEFAULT));
+	return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    private Bitmap getBitmap(Drawable drawable){
+	Bitmap bitmap = null;
+
+	if (drawable instanceof BitmapDrawable) {
+	    BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+	    if(bitmapDrawable.getBitmap() != null) {
+		return bitmapDrawable.getBitmap();
+	    }
+	}
+
+	if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+	    bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+	} else {
+	    bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+	}
+
+	Canvas canvas = new Canvas(bitmap);
+	drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+	drawable.draw(canvas);
+	return bitmap;
+    }
+
+    private void getIcon(CallbackContext callbackContext, String packageName){
+	try {
+	    callbackContext.success(bitmapToBase64(getBitmap(pm.getApplicationIcon(packageName))));
+	} catch (PackageManager.NameNotFoundException e) {
+	    callbackContext.error(e.toString());
+	}
+    }
+
+    private void getApps(CallbackContext callbackContext, int flags){
+	JSONArray output = new JSONArray();
 	CordovaActivity activity = (CordovaActivity) this.cordova.getActivity();
-	final PackageManager pm = activity.getPackageManager();
-	//get a list of installed apps.
+	pm = activity.getPackageManager();
+
 	List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
 	for (int i = 0;i < packages.size(); i++){
-	    output += packages.toString();
+	    JSONObject application = new JSONObject();
+	    try {
+		application.put("packageName", packages.get(i).packageName);
+	    } catch (JSONException e) {
+		callbackContext.error(e.toString());
+		e.printStackTrace();
+	    }
+	    try {
+		application.put("flags", packages.get(i).flags);
+	    } catch (JSONException e) {
+		callbackContext.error(e.toString());
+		e.printStackTrace();
+	    }
+	    output.put(application);
 	}
 	callbackContext.success(output);
     }
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	if (action.equals("echo")) {
-	    String message = args.getString(0);
-	    this.echo(message, callbackContext);
-	    return true;
-	}
-	if (action.equals("apps")) {
-	    this.getApps(callbackContext);
-	}
-	return false;
-    }
-
-    private void echo(String message, CallbackContext callbackContext) {
-	if (message != null && message.length() > 0) {
-	    callbackContext.success(message);
-	} else {
-	    callbackContext.error("Expected one non-empty string argument.");
-	}
-    }
+                @Override
+		public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		    if (action.equals("icon")) {
+			this.getIcon(callbackContext, args.getString(0));
+		    }
+		    if (action.equals("apps")) {
+			this.getApps(callbackContext, 0);
+		    }
+		    if (action.equals("echo")){
+			callbackContext.success(args.getString(0));
+		    }
+		    return false;
+		}
 }
--- sugarizeros.js
+++ sugarizeros.js
@@ -2,15 +2,26 @@ var sugarizerOS = {};
 var exec = require('cordova/exec');
 
 sugarizerOS.init = function(){
-    console.log("SugarizerOS initialized");
+    if (window){
+	window.sugarizerOS = sugarizerOS;
+	console.log("SugarizerOS initialized");
+    }
+    else{
+	console.log("No window to initialize sugarizerOS");
+    }
 }
 
-sugarizerOS.getAndroidApplications = function(onSuccess, onFailure){
-    exec(onSuccess, onFailure, "SugarizerOSPlugin", "apps", []);
+sugarizerOS.echo = function(onSuccess, onFailure, string){
+    exec(onSuccess, onFailure, "SugarizerOSPlugin", "echo", string);
+}
+
+sugarizerOS.getAndroidApplications = function(onSuccess, onFailure, flags){
+    exec(onSuccess, onFailure, "SugarizerOSPlugin", "apps", flags);
+}
+
+sugarizerOS.getAndroidApplicationIcon = function(onSuccess, onFailure, packageName){
+    exec(onSuccess, onFailure, "SugarizerOSPlugin", "icon", packageName);
 }
 
 sugarizerOS.init();
 
-if (window){
-    window.sugarizerOS = sugarizerOS;
-}
